9f50cbec330633a57dee76b63d8d40cc8552c57c
Anika Hakim <hakimanika3529@gmail.com>
2026-07-01 19:17:43 +0000

    test2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, flash, redirect, request
+import git
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -6,6 +7,12 @@ app.config['SECRET_KEY'] = 'apples123'
 app.debug = True
 toolbar = DebugToolbarExtension(app)
 
-@app.route('/')
-def index():
-    return 'Hello, world!'
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/CHANGE_TO_PYTHON_ANYWHERE_USERNAME/CHANGE_TO_GITHUB_REPO_NAME')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
