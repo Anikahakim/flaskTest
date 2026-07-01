8f639ae8ee5e7006f7f45e90602797db5e780b0e
Anika Hakim <hakimanika3529@gmail.com>
2026-07-01 19:19:45 +0000

    test3

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ toolbar = DebugToolbarExtension(app)
 @app.route("/update_server", methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('/home/CHANGE_TO_PYTHON_ANYWHERE_USERNAME/CHANGE_TO_GITHUB_REPO_NAME')
+        repo = git.Repo('/home/flaskTestDeployed/flaskTest')
         origin = repo.remotes.origin
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
